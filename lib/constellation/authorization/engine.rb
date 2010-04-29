e110e49a6cdc1f57a18939f29864d638371a378e
Ian Terrell <ian.terrell@gmail.com>
2010-04-28 23:02:29 -0700

    tiny API and default tweaks

--- lib/constellation/authorization/engine.rb
+++ lib/constellation/authorization/engine.rb
@@ -6,8 +6,8 @@ module Constellation
     class Engine < Rails::Engine
       config.constellation = ActiveSupport::OrderedOptions.new unless config.respond_to? :constellation
       config.constellation.authorization = ActiveSupport::OrderedOptions.new
-      config.constellation.authorization.mixin = :default
-      config.constellation.authorization.login_required_redirection = { :controller => 'session', :action => 'new' }
+      config.constellation.authorization.implementation = :default
+      config.constellation.authorization.login_required_redirection = { :controller => 'user_sessions', :action => 'new' }
       config.constellation.authorization.permission_denied_redirection = ''
       config.constellation.authorization.store_location_method = :store_location
       config.constellation.authorization.current_user_method = :current_user
@@ -16,7 +16,7 @@ module Constellation
       initializer "constellation.authorization.default" do |app|
         ActionController::Base.send :include, Constellation::Authorization::Base
         ActionView::Base.send :include, Constellation::Authorization::Base::ControllerInstanceMethods
-        if app.config.constellation.authorization.mixin == :default
+        if app.config.constellation.authorization.implementation == :default
           ActiveRecord::Base.send :include, 
             Constellation::Authorization::Default::UserExtensions, 
             Constellation::Authorization::Default::ModelExtensions 
